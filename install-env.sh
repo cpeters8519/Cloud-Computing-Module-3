d78577f485bb8c03797b7c481ba55f5a9592e351
cpeters8519 <cpeters8519@gmail.com>
2026-04-05 13:34:12 -0500

    update install-esv.sh

--- install-env.sh
+++ install-env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Install dependecies here:
+# Install dependencies here:
 
 ##############################################################################
 # Installing Nginx
@@ -13,4 +13,13 @@ sudo apt install nginx unzip -y
 ##############################################################################
 sudo systemctl enable nginx
 sudo systemctl start nginx
+sleep 10
 
+# Create a simple default page so HTTP requests return 200
+echo "<html>
+<head><title>Module 03</title></head>
+<body>
+<h1>Module 03 EC2 Instance</h1>
+<p>This page is served from $(hostname)</p>
+</body>
+</html>" | sudo tee /var/www/html/index.html
